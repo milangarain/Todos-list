d0944f1fa679e47936a2cec233083288be7b176b
milangarain <56685842+milangarain@users.noreply.github.com>
2021-12-31 12:23:26 +0530

    Deleting redundant things and adding dummy list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2> Add your list Here: </h2>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> Add your todo list below: </h2>
+      <ul>
+        <li> Fix Sonar </li>
+        <li> Complete this code </li>
+      </ul>
     </div>
   );
 }
