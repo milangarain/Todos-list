3a19c6e60f2742253930035ab8c699ccb707a1c0
Milan Garain <milangarain1999@gmail.com>
2023-07-22 16:57:07 +0530

    update default todos

--- src/App.js
+++ src/App.js
@@ -8,17 +8,9 @@ import AddTodoItem from "./MyComponents/AddTodoItem";
 function App() {
   const [todoList, setTodoList] = useState([{
     SNo: 1,
-    name: "Complete IPSF form",
-    desp: "Complete IPSF form by 5th Junuary otherwise your will deduct accordingly"
-  }, {
-    SNo: 2,
-    name: "Do investments",
-    desp: "Do investment in order to decrese taxable income under 5 lakh to avoid tax deduction"
-  }, {
-    SNo: 3,
-    name: "New Year Resolutions",
-    desp: "List down new year resolutions and follow strictly"
-  }]);
+    name: "default Todo",
+    desp: "Delete this and start your journey by adding actual todo item"
+  }])
 
   const [sNO, setSNo] = useState(4);
 
